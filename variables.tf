91e65c5fb492c41f810680ae8cabc1d3bdaf38a3
James Thomas <mrcloudmustache@gmail.com>
2025-04-13 11:24:37 -0500

    Removed defaults

--- variables.tf
+++ variables.tf
@@ -1,32 +1,32 @@
 variable "cidr_block" {
     description = "The network and prefix of the vpc"
     type = string
-    default = "10.0.0.0/16"
+    # default = "10.0.0.0/16"
   
 }
 
 variable "vpc_name" {
     description = "The name of the vpc"
     type = string
-    default = "aws-vpc-hello-world"
+    # default = "aws-vpc-hello-world"
   
 }
 
 variable "environment" {
     description = "The name of the environment"
     type = string
-    default = "dev"
+    # default = "dev"
 
-    validation {
-      condition = var.environment == "prod" || var.environment == "dev"
-      error_message = "The environment name must be 'prod' or 'dev'."
-    }
+    # validation {
+    #   condition = var.environment == "prod" || var.environment == "dev"
+    #   error_message = "The environment name must be 'prod' or 'dev'."
+    # }
   
 }
 
 variable "region"  {
     description = "The name of the AWS region"
     type = string
-    default = "us-east-1"
+    # default = "us-east-1"
   
 }
