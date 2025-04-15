3f000486105370a2e13563bafbbe69bf4c15cb07
James Thomas <mrcloudmustache@gmail.com>
2025-04-15 11:39:07 -0500

    Updated README.md

--- outputs.tf
+++ outputs.tf
@@ -5,49 +5,49 @@ output "vpc_id" {
 }
 
 output "private_subnet1_id" {
-    description = "The ID of the VPC"
+    description = "The ID of the subnet"
     value = aws_subnet.private1.id
   
 }
 
 output "private_subnet2_id" {
-    description = "The ID of the VPC"
+    description = "The ID of the subnet"
     value = aws_subnet.private2.id
   
 }
 
 output "public_subnet1_id" {
-    description = "The ID of the VPC"
+    description = "The ID of the subnet"
     value = aws_subnet.public1.id
   
 }
 
 output "public_subnet2_id" {
-    description = "The ID of the VPC"
+    description = "The ID of the subnet"
     value = aws_subnet.public2.id
   
 }
 
 output "private_subnet1_cidr" {
-    description = "The ID of the VPC"
+    description = "The cidr block of the subnet"
     value = aws_subnet.private1.cidr_block
   
 }
 
 output "private_subnet2_cidr" {
-    description = "The ID of the VPC"
+    description = "The cidr block of the subnet"
     value = aws_subnet.private2.cidr_block
   
 }
 
 output "public_subnet1_cidr" {
-    description = "The ID of the VPC"
+    description = "The cidr block of the subnet"
     value = aws_subnet.public1.cidr_block
   
 }
 
 output "public_subnet2_cidr" {
-    description = "The ID of the VPC"
+    description = "The cidr block of the subnet"
     value = aws_subnet.public2.cidr_block
   
 }
